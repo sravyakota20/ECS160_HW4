2f34c1a35c996cd8ebde507bae2bedda9202f1ca
Sravya Kota <sskota@ucdavis.edu>
2025-02-20 22:25:09 -0500

    Refactored to use Redis

--- src/main/java/com/ecs160/hw2/Session.java
+++ src/main/java/com/ecs160/hw2/Session.java
@@ -1,5 +1,9 @@
-package com.ecs160.hw2.persistence;
+package com.ecs160.hw2;
 
+import com.ecs160.hw2.persistence.Persistable;
+import com.ecs160.hw2.persistence.PersistableField;
+import com.ecs160.hw2.persistence.PersistableId;
+import com.ecs160.hw2.persistence.PersistableListField;
 import redis.clients.jedis.Jedis;
 import java.lang.reflect.Field;
 import java.util.*;
@@ -18,6 +22,8 @@ public class Session {
     public void add(Object obj) {
         if (obj.getClass().isAnnotationPresent(Persistable.class)) {
             sessionObjects.add(obj);
+        } else {
+        throw new IllegalArgumentException("Class is not persistable: " + obj.getClass().getName());
         }
     }
 
@@ -28,46 +34,74 @@ public class Session {
     }
 
     private void persistObject(Object obj) {
-        String key = getPrimaryKeyValue(obj);
-        Map<String, String> fields = new HashMap<>();
+        Class<?> clazz = obj.getClass();
+        String postKey = "post:" + getId(obj);
 
-        for (Field field : obj.getClass().getDeclaredFields()) {
+        for (Field field : clazz.getDeclaredFields()) {
             field.setAccessible(true);
             try {
-                if (field.isAnnotationPresent(PersistableField.class)) {
-                    fields.put(field.getName(), field.get(obj).toString());
+                if (field.isAnnotationPresent(PersistableField.class) || field.isAnnotationPresent(PersistableId.class)) {
+                    jedis.hset(postKey, field.getName(), String.valueOf(field.get(obj)));
                 } else if (field.isAnnotationPresent(PersistableListField.class)) {
                     List<?> list = (List<?>) field.get(obj);
-                    fields.put(field.getName(), serializeList(list));
+                    if (list != null) {
+                        String ids = String.join(",", list.stream().map(item -> String.valueOf(getId(item))).toArray(String[]::new));
+                        jedis.hset(postKey, "replyIds", ids);
+                    }
                 }
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
             }
         }
-        jedis.hmset(key, fields);
+    }
+
+    private int getId(Object obj) {
+        try {
+            for (Field field : obj.getClass().getDeclaredFields()) {
+                if (field.isAnnotationPresent(PersistableId.class)) {
+                    field.setAccessible(true);
+                    return (int) field.get(obj);
+                }
+            }
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        throw new IllegalArgumentException("No @PersistableId found in " + obj.getClass().getName());
     }
 
     public Object load(Object obj) {
-        String key = getPrimaryKeyValue(obj);
-        Map<String, String> storedData = jedis.hgetAll(key);
-        
-        if (storedData.isEmpty()) return null;
-        
+        String postKey = "post:" + getId(obj);
+        Map<String, String> data = jedis.hgetAll(postKey);
+
+        if (data.isEmpty()) return null;
+
         try {
-            Object loadedObj = obj.getClass().getDeclaredConstructor().newInstance();
-            for (Field field : obj.getClass().getDeclaredFields()) {
+            Class<?> clazz = obj.getClass();
+            Object instance = clazz.getDeclaredConstructor().newInstance();
+
+            for (Field field : clazz.getDeclaredFields()) {
                 field.setAccessible(true);
-                if (field.isAnnotationPresent(PersistableField.class)) {
-                    field.set(loadedObj, castValue(field.getType(), storedData.get(field.getName())));
+                if (field.isAnnotationPresent(PersistableField.class) || field.isAnnotationPresent(PersistableId.class)) {
+                    field.set(instance, castValue(field.getType(), data.get(field.getName())));
                 } else if (field.isAnnotationPresent(PersistableListField.class)) {
-                    field.set(loadedObj, deserializeList(field.getType(), storedData.get(field.getName())));
+                    List<Post> replies = new ArrayList<>();
+                    if (data.containsKey("replyIds")) {
+                        for (String id : data.get("replyIds").split(",")) {
+                            if (!id.isEmpty()) {
+                                Post reply = new Post();
+                                reply.setPostId(Integer.parseInt(id));
+                                replies.add(reply);
+                            }
+                        }
+                    }
+                    field.set(instance, replies);
                 }
             }
-            return loadedObj;
+            return instance;
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
-        return null;
     }
 
     private String getPrimaryKeyValue(Object obj) {
